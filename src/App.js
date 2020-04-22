773f494df0b4593f6c9bf25da05360e5829df12b
y3-saito <kacco.design@gmail.com>
2020-04-22 17:40:48 +0900

    追加しました

--- src/App.js
+++ src/App.js
@@ -1,64 +1,28 @@
-import React from 'react';
-// import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 
-// class App exports Component {
-//   render() {
-//     return (
-//     <React.Fragment> 
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => { console.log("I am yuta") }} />
-//     </React.Fragment>
-//     )
-//   }
-// }
+const App = () => (<Counter></Counter>)
 
-// function App() {
-//   return (
-//     // ↓ React.Fragmentは無駄なダグを表示させないための記述
-//     <React.Fragment> 
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => { console.log("I am yuta") }} />
-//     </React.Fragment>
-//   );
-// }
-// function App() {
-//   const greeting = "Hi!!";
-//   const dom = <h1 className="foo">{greeting}</h1>;
-//   return dom;
-// }
-
-// function App() {
-//   return React.createElement(
-//     "h1",
-//     null,
-//     "Hello,wold!!"
-//   );
-// }
-
-const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hama", age: 5 },
-    {name: "Yamada"}
-
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = { count: 0 }
+  }
+  hansdlePlusButton = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+  hansdleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+  render() {
+    return (
+    <React.Fragment>
+      <div>count:{ this.state.count}</div>
+      <button onClick={this.hansdlePlusButton}>+1</button>
+      <button onClick={this.hansdleMinusButton}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age}</div>
-}
-
-User.defaultProps = {
-  age: 1
-}
 
 export default App;
